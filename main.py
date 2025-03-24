482122f007c87e4c51c238c9458e0c997a8d0ba3
sana khan <88804450+sanatiwary@users.noreply.github.com>
2025-03-23 20:53:39 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -15,7 +15,53 @@ print(data.isnull().sum())
 
 # data preprocessing
 print("median of age column %.2f"% (data["Age"].median(skipna=True)))
-
 data["Age"].fillna(data["Age"].median(skipna=True), inplace=True)
+print(data.isnull().sum())
+
+print(data["Embarked"].value_counts().idxmax())
+data["Embarked"].fillna(data["Embarked"].value_counts().idxmax(), inplace=True)
+print(data.isnull().sum())
+
+#dropping unnecessary data
+data.drop("Cabin", axis=1, inplace=True)
+data.drop("PassengerId", axis=1, inplace=True)
+data.drop("Ticket", axis=1, inplace=True)
+data.drop("Name", axis=1, inplace=True)
+
+data["TravelAlone"] = np.where((data["SibSp"] + data["Parch"]) > 0, 0, 1)
+
+data.drop("SibSp", axis=1, inplace=True)
+data.drop("Parch", axis=1, inplace=True)
 
 print(data.isnull().sum())
+print(data.head())
+
+# label encoder replaces string data with numerical values for machine learning processes
+from sklearn import preprocessing
+le = preprocessing.LabelEncoder()
+
+data["Sex"] = le.fit_transform(data["Sex"])
+data["Embarked"] = le.fit_transform(data["Embarked"])
+
+print(data.head())
+
+x = data[["Pclass", "Sex", "Age", "Fare", "Embarked", "TravelAlone"]]
+y = data["Survived"]
+
+xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.2, random_state=2)
+
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression()
+
+lr.fit(xTrain, yTrain)
+
+yTestPredict = lr.predict(xTest)
+
+from sklearn.metrics import confusion_matrix
+matrix = confusion_matrix(yTest, yTestPredict)
+
+sns.heatmap(matrix, annot=True, fmt="d")
+plt.title("matrix")
+plt.xlabel("predicted")
+plt.ylabel("actual")
+plt.show()
